2c2d7bc2f158f6c292a2a7fee2c4d9b38e65a916
Alex Zaver <zaverclub@gmail.com>
2023-11-15 03:20:37 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+const timeout = setTimeout(() => console.log('Hello playwright'), 1000)
+
+let result: number = timeout + 15
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+});
+</script>
